4becc6990f4b11fa84a2809628d678a8c2542d80
hunny <hunny@katchintech.com>
2024-08-10 09:46:19 +0530

    bridge header for ios

--- src/Dashboard/exchange/crypto-exchange-front-end-main/src/components/classic.js
+++ src/Dashboard/exchange/crypto-exchange-front-end-main/src/components/classic.js
@@ -140,7 +140,8 @@ const getOffersData = async () => {
         textAlign: 'center',
         color:"#fff",
         flex: 1,
-        marginLeft:wp(13)
+        marginLeft:wp(13),
+        marginTop: '9%'
       }}>Bridge</Text>
 
       {/* Right Image and Menu Icon */}
